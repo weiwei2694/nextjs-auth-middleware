17fa4b4afaa672333e0ac93bf80229613d6f8505
Arya Priema <99aryaprima@gmail.com>
2023-08-24 19:31:47 +0700

    feat: /settings
    create interface

---
+++ src/app/(protect)/settings/page.tsx
@@ -0,0 +1,11 @@
+const SettingsPage = () => {
+    return (
+        <section>
+            <h1 className="text-xl sm:text-3xl font-extrabold tracking-wider text-black">
+                Settings
+            </h1>
+        </section>
+    );
+};
+
+export default SettingsPage;
