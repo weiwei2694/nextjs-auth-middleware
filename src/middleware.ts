7d92edf417310cc13cc98f3ddd3047573ba7f188
Arya Priema <99aryaprima@gmail.com>
2023-08-24 20:54:59 +0700

    [docs/feat]: middleware - Add comments and documentation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,37 @@
+// Import the NextResponse module from next/server
 import { NextResponse } from "next/server";
+// Imports the NextRequest type from next/server
 import type { NextRequest } from "next/server";
 
+// Middleware function that receives a req object (NextRequest)
 export function middleware(req: NextRequest) {
+    // Get the "session" value from the cookie on request
     const session = req.cookies.get("session");
 
+    // Function to perform a redirect to a specific URL
     const redirect = (url: string, reqUrl: string) => {
         return NextResponse.redirect(new URL(url, reqUrl));
     };
+    // Function to check if the request URL starts with a specified string
     const startsWith = (url: string) => {
         return req.nextUrl.pathname.startsWith(url);
     };
 
+    // Checks if no session exists
     if (!session) {
+        // Uses switch case to check for multiple URLs that start with a certain string
         switch (true) {
+            // If the request URL starts with /dashboard, /profile, or /settings
             case startsWith("/dashboard"):
             case startsWith("/profile"):
             case startsWith("/settings"):
+                // Performs a redirect to the authentication page
                 return redirect("/auth/login", req.url);
         }
     }
 
-    if (startsWith('/auth/login')) return redirect("/dashboard", req.url);
+    // If the request URL starts with /auth/login
+    if (startsWith("/auth/login"))
+        // Make a redirect to the dashboard page after logging in
+        return redirect("/dashboard", req.url);
 }
