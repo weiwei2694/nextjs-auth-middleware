20c4b36d8a1aa92ef15c778de5106f9f70b4ad2f
Arya Priema <99aryaprima@gmail.com>
2023-08-24 20:00:58 +0700

    [update/feat]: /auth/login
    handle user login

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,20 +1,51 @@
 "use client";
 import { FormEvent } from "react";
 
+import { useRouter } from "next/navigation";
+import { setCookie } from "cookies-next";
+
 const LoginForm = () => {
+    const router = useRouter();
+
     const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        // TODO: handle user login
-    }
+        // @ts-ignore
+        const email = e.target.email.value;
+        // @ts-ignore
+        const password = e.target.password.value;
 
-  return (
-    <form onSubmit={handleLogin} className="flex flex-col gap-4">
-        <input type="email" required name="email" id="email" placeholder="email" className="input" />
-        <input type="password" required name="password" id="password" placeholder="password" className="input" />
-        <button type="submit" className="button__submit">Login</button>
-    </form>
-  )
-}
+        setCookie("session", {
+            email,
+            password,
+            token: new Date().getTime().toString(),
+        });
+        router.push("/dashboard");
+    };
 
-export default LoginForm
+    return (
+        <form onSubmit={handleLogin} className="flex flex-col gap-4">
+            <input
+                type="email"
+                required
+                name="email"
+                id="email"
+                placeholder="email"
+                className="input"
+            />
+            <input
+                type="password"
+                required
+                name="password"
+                id="password"
+                placeholder="password"
+                className="input"
+            />
+            <button type="submit" className="button__submit">
+                Login
+            </button>
+        </form>
+    );
+};
+
+export default LoginForm;
