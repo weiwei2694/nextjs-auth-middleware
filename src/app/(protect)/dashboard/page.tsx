9eacd9e2b3be22990c8be30ec3a3c3897e6cf38b
Arya Priema <99aryaprima@gmail.com>
2023-08-24 19:29:56 +0700

    feat: /dashboard
    create interface

---
+++ src/app/(protect)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = () => {
+    return (
+        <section>
+            <h1 className="text-xl sm:text-3xl font-extrabold tracking-wider text-black">
+                Dashboard
+            </h1>
+        </section>
+    );
+};
+
+export default DashboardPage;
