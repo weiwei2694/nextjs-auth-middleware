67a5c7aaa97fd390c8a86483342cc1ec99a17e79
Arya Priema <99aryaprima@gmail.com>
2023-08-24 19:31:07 +0700

    C:/Program Files/Git/profile
    create interface

---
+++ src/app/(protect)/profile/page.tsx
@@ -0,0 +1,11 @@
+const ProfilePage = () => {
+    return (
+        <section>
+            <h1 className="text-xl sm:text-3xl font-extrabold tracking-wider text-black">
+                Profile
+            </h1>
+        </section>
+    );
+};
+
+export default ProfilePage;
